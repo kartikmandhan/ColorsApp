6d83f3c9cac5f8d993e57e2946b7f57f637710ef
Kartik <kartikmandhan134@gmail.com>
2021-04-16 17:10:18 +0530

    Modal SetUp done

--- screens/HomePage.js
+++ screens/HomePage.js
@@ -1,71 +1,97 @@
 import PalettePreview from "../components/PalettePreview";
-import React, { useState, useEffect, useCallback } from "react";
-import { StyleSheet, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  FlatList,
+  RefreshControl,
+  TouchableOpacity,
+  Modal,
+  Text,
+  View,
+} from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 const HomePage = ({ navigation }) => {
-  const RAINBOW = [
-    { colorName: "Red", hexCode: "#FF0000" },
-    { colorName: "Orange", hexCode: "#FF7F00" },
-    { colorName: "Yellow", hexCode: "#FFFF00" },
-    { colorName: "Green", hexCode: "#00FF00" },
-    { colorName: "Violet", hexCode: "#8B00FF" },
-  ];
-
-  const SDS_PALETTTE = [
-    { colorName: "Red", hexCode: "#c02d28" },
-    { colorName: "Black", hexCode: "#3e3e3e" },
-    { colorName: "Grey", hexCode: "#8a8a8a" },
-    { colorName: "White", hexCode: "#ffffff" },
-    { colorName: "Orange", hexCode: "#e66225" },
-  ];
-  const SOLARIZED = [
-    { colorName: "Base03", hexCode: "#002b36" },
-    { colorName: "Base02", hexCode: "#073642" },
-    { colorName: "Base01", hexCode: "#586e75" },
-    { colorName: "Base00", hexCode: "#657b83" },
-    { colorName: "Base0", hexCode: "#839496" },
-    { colorName: "Base1", hexCode: "#93a1a1" },
-    { colorName: "Base2", hexCode: "#eee8d5" },
-    { colorName: "Base3", hexCode: "#fdf6e3" },
-    { colorName: "Yellow", hexCode: "#b58900" },
-    { colorName: "Orange", hexCode: "#cb4b16" },
-    { colorName: "Red", hexCode: "#dc322f" },
-    { colorName: "Magenta", hexCode: "#d33682" },
-    { colorName: "Violet", hexCode: "#6c71c4" },
-    { colorName: "Blue", hexCode: "#268bd2" },
-    { colorName: "Cyan", hexCode: "#2aa198" },
-    { colorName: "Green", hexCode: "#859900" },
-  ];
-  const PALETTES = [
-    {
-      colors: SOLARIZED,
-      paletteName: "Solarized",
-    },
-
-    {
-      colors: RAINBOW,
-      paletteName: "Rainbow",
-    },
-
-    {
-      colors: SDS_PALETTTE,
-      paletteName: "SDS's Palette",
-    },
-  ];
+  const [palette, setPalette] = useState([]);
+  const [isRefreshing, setIsRefreshing] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const URL = "https://color-palette-api.kadikraman.now.sh/palettes";
+  const handleFetchPalettes = async () => {
+    const response = await fetch(URL);
+    if (response.ok) {
+      const palettes = await response.json();
+      setPalette(palettes);
+    }
+  };
+  useEffect(() => {
+    handleFetchPalettes();
+  }, []);
+  const handleRefresh = async () => {
+    setIsRefreshing(true);
+    await handleFetchPalettes();
+    setIsRefreshing(false);
+  };
   return (
-    <FlatList
-      style={{ backgroundColor: "white" }}
-      data={PALETTES}
-      keyExtractor={(item) => item.paletteName}
-      renderItem={({ item }) => (
-        <PalettePreview
-          palette={item}
-          onPress={() => navigation.navigate("ColourPalette", item)}
-        />
-      )}
-    />
+    <View style={{ backgroundColor: "#fff" }}>
+      <Modal visible={isModalVisible} animationType="slide">
+        <View style={styles.container}>
+          <View style={styles.topContainer}>
+            <MaterialIcons
+              name="close"
+              size={28}
+              onPress={() => {
+                setIsModalVisible(false);
+              }}
+            />
+          </View>
+        </View>
+      </Modal>
+      <FlatList
+        ListHeaderComponent={() => {
+          return (
+            <TouchableOpacity onPress={() => setIsModalVisible(true)}>
+              <Text style={styles.button}> + Add a color scheme</Text>
+            </TouchableOpacity>
+          );
+        }}
+        data={palette}
+        keyExtractor={(item) => item.paletteName}
+        renderItem={({ item }) => (
+          <PalettePreview
+            palette={item}
+            onPress={() => navigation.navigate("ColourPalette", item)}
+          />
+        )}
+        refreshControl={
+          <RefreshControl refreshing={isRefreshing} onRefresh={handleRefresh} />
+        }
+      />
+    </View>
   );
 };
 
 export default HomePage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    fontSize: 20,
+    color: "dodgerblue",
+    textAlign: "center",
+    fontWeight: "bold",
+    padding: 20,
+    borderBottomWidth: 1,
+    borderColor: "#eee",
+  },
+  container: {
+    flex: 1,
+    paddingVertical: 8,
+  },
+  topContainer: {
+    height: "5%",
+    padding: 10,
+    borderBottomWidth: 1,
+    textAlignVertical: "center",
+    borderColor: "#eee",
+    justifyContent: "center",
+    alignItems: "flex-end",
+  },
+});
