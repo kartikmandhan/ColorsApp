275df6da9ae8b7dcf1d679a5f2a6399289d58c36
Kartik <kartikmandhan134@gmail.com>
2021-04-16 15:30:47 +0530

    stackNavigator setUp

---
+++ screens/ColourPalette.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, Text, FlatList } from "react-native";
+import ColorBox from "../components/ColorBox";
+
+const ColourPalette = (props) => {
+  //   console.log(props.route.params);
+  const { paletteName, colors } = props.route.params;
+  return (
+    <FlatList
+      style={styles.container}
+      data={colors}
+      keyExtractor={(item) => item.colorName}
+      showsVerticalScrollIndicator={false}
+      ListHeaderComponent={<Text style={styles.subHeading}>{paletteName}</Text>}
+      renderItem={({ item }) => {
+        return <ColorBox color={item.colorName} hexValue={item.hexCode} />;
+      }}
+    />
+  );
+};
+
+export default ColourPalette;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 8,
+  },
+  subHeading: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 14,
+    padding: 10,
+    fontFamily: "",
+  },
+});
